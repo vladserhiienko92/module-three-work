8de1b4c24c7c9638bb290be80e5f96e81fe38605
Vlad Serhiienko <vladserhiienko92@gmail.com>
2017-02-11 19:55:27 +0200

    push

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,39 +1,51 @@
 $(document).ready(function() {
 
-    $(window).scroll(function() {
-        var fixed_head_height = $(".navbar").height();
-        var winTop = $(window).scrollTop();
-        if (winTop >= 211) {
-            $("nav").addClass("sticky-header");
-            $(".section-header").css("padding-top", fixed_head_height + "px");
+    $(document).on("click", ".mobile-icon", function() {
+        $(".mobile-menu").slideToggle();
+        if ($(".mobile-icon").hasClass("active")) {
+            $(".mobile-icon").removeClass("active")
         } else {
-            $("nav").removeClass("sticky-header");
-            $(".section-header").css("padding-top", 0);
+            $(".mobile-icon").addClass("active");
         }
+        
     });
 
-    $(".client-slider").slick({
-        "arrows": false,
+    $('.work-slider').slick({
         "dots": true,
-        "autoplay": true,
-        "draggable": false
     });
 
-    $(document).on("click",".navbar-nav__link", function(e) {
+    $('.team-slider').slick({
+
+    });
+
+    $(document).on("click",".menu-item a", function(e) {
         e.preventDefault();
         var id  = $(this).attr('href'),
         top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
     });
 
-    $(".nav").singlePageNav({
-        offset: 0,                  // Offset from top
-        currentClass: 'current',    // Class added to menu link when section is active
-        currentThreshold: 0,        // Threshold for triggering current section action
-        duration: 500,              // Duration of scroll animation in milliseconds 
-        effect: 'swing',            // Effect for scroll animation
-        started: function (){},     // Callback at start of animation
-        finished: function (){}     // Callback after animation is finished
+    $(document).on("click", ".to-the-top", function() {
+
+        var delay = 1000;
+
+        $('body, html').animate({
+            scrollTop: 0
+        }, delay);
+
+    });
+
+    $('.to-the-top').fadeOut();
+
+    $(document).scroll(function() {
+        var top_show = 250;
+
+        if ($(this).scrollTop() > top_show) {
+            $('.to-the-top').fadeIn();
+        } else {
+            $('.to-the-top').fadeOut();
+        }
+
     });
 
 });
